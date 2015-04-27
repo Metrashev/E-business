1224303b422db6ccb54e335124ca51832dca70a0
Metrashev <vladimir.metrashev@gmail.com>
2015-04-27 20:14:59 +0300

    Create repository

---
+++ E-business/E-business/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(E_business.Startup))]
+namespace E_business
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
